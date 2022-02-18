10b63c46d22f685ff7d65fb716019823acd88f3d
Mert Kılıçaslan <94853536+mertkilicaslan@users.noreply.github.com>
2022-02-19 01:39:37 +0300

    Update favoriteseries.h

--- THE6 - Favorite TV Series/favoriteseries.h
+++ THE6 - Favorite TV Series/favoriteseries.h
@@ -26,7 +26,7 @@ class FavoriteSeries{
 		FavoriteSeries(string, string);								// Parameter Constructor
     
 		string getFullName() const;									// Returns the full name (name&surname) of the user 
-		void addSeries(const series &);								// Adds TV series to the favorite list
+		void addSeries(const series &);								// Adds TV series to the favourite list
 		bool ifExists(int seriesCode);								// Checks if the serie in the favorite list or not
 		bool isListEmpty() const;									// Returns true if the favorite list is empty
 		void dropSeriesSorted(int);									// Drops the TV series with the given TV serie code from the vector (favSeries) while keeping the vector sorted
